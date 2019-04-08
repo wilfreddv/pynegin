41d1a66aeb5f079340d6795e84e1d01b4a9a5de2
Wilfred <26687641+wilfred-kun@users.noreply.github.com>
2019-04-08 22:31:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+with open("README.md", "rb") as f:
+    long_desc = str(f.read())
+
+
+PACKAGES = ['pynegin',
+            'pynegin.components',
+            'pynegin.constants'
+           ]
+
 
 setup(
     name="pynegin",
-    version="0.1",
+    version="0.2",
     description="Pure Python Game Engine On Top Of PyGame",
+    long_description=long_desc,
     author="Wilfred",
-    author_email="_",
-    packages=["pynegin"],
+    author_email="wilfred@programmer.net",
+    packages=PACKAGES,
     install_requires=["pygame", "Pillow"],
     url="http://wilfred-kun.github.io/pynegin.html",
     classifiers=[
