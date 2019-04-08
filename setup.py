255ac98e2dc79bd2d2655802cadc20d265e6206d
Wilfred <26687641+wilfred-kun@users.noreply.github.com>
2019-04-08 19:49:28 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pynegin",
+    version="0.1",
+    description="Pure Python Game Engine On Top Of PyGame",
+    author="Wilfred",
+    author_email="_",
+    packages=["pynegin"],
+    url="https://github.com/wilfred-kun/pynegin"
+)
