5990abcfdc76e589dbf8c32d2b1094b78d70721d
Wilfred <26687641+wilfred-kun@users.noreply.github.com>
2019-06-20 00:07:50 +0200

    Add lines (they're funky)

--- game.py
+++ game.py
@@ -1,7 +1,8 @@
-from pynegin.components import text, menu, image, sprite, inputField
+from pynegin.components import text, menu, image, sprite, inputField, line, component
 from pynegin.constants.colors import COLORS
 from pynegin.gameLogic import GameLogic
 import pygame
+from pygame import Surface
 
 
 # class MainMenu(menu.Menu):
@@ -19,6 +20,21 @@ import pygame
 #         self.K_PREV = pygame.K_w
 #         self.centerHorizontal()
 
+class Lines(component.Component):
+    def __init__(self, container):
+        self.lines = []
+        self.lines.append(line.Line(container, (10,10), (container.getWidth()-10, 10), 10, COLORS.GREEN))
+        self.lines.append(line.Line(container, (container.getWidth()-10,10), (container.getWidth()-10, container.getHeight()-10), 10, COLORS.WHITE))
+        self.lines.append(line.Line(container, (10,10), (10, container.getHeight()-10), 10, COLORS.BLUE))
+        self.lines.append(line.Line(container, (10,container.getHeight()-10), (container.getWidth()-10, container.getHeight()-10), 10, COLORS.RED))
+        size = container.getWidth(), container.getHeight()
+        surface = Surface(size)
+        super().__init__(container, surface=surface, size=size)
+
+    def show(self, surf):
+        for line in self.lines:
+            line.show(surf)
+
 
 class Game(GameLogic):
     def __init__(self, window):
@@ -32,8 +48,12 @@ class Game(GameLogic):
 
         self.textField = inputField.InputField(window, textSize=30, color=COLORS.WHITE)
         self.t = text.Text(window, text="Hello!", textSize=50, color=COLORS.WHITE)
-        super().__init__(window, self.textField)
+        ctx = Lines(window)
+
+        super().__init__(window, ctx)
 
 
     def input(self):
-        self.textField.updateInput(self.window.getPressedKeys())
+        #self.textField.updateInput(self.window.getPressedKeys())
+
+        ...
